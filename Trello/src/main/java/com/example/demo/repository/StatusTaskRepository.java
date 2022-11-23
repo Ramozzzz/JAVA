7bd1c21b381a9a6455f0adb62caa1b96f0e4cbe3
Ramozzzz <71695466+Ramozzzz@users.noreply.github.com>
2022-11-23 06:53:29 +0300

    оставил только важное

--- Trello/src/main/java/com/example/demo/repository/StatusTaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.StatusTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusTaskRepository extends JpaRepository<StatusTask, Long> {
-    StatusTask findStatusTaskByRowId(long id);
-}
